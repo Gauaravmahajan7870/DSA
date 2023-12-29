eebc6053036d2a443a4c24de7ff0f0486198af0d
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2023-12-29 23:05:17 +0530

    swap num

---
+++ begin with cpp/swapnum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  float num1,num2,sum,average;
+  cout<<"enter a num";
+  cin>>num1;
+  cin>>num2;
+
+  sum=num1+num2;
+  average =sum/2;
+  cout<<"sum"<<sum<<endl;
+  cout<<"average"<<average<<endl;
+return 0;
+}
