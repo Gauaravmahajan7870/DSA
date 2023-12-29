8ca4b5a377a041ace5ec4e5ca099085f276ace47
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2023-12-29 21:27:04 +0530

    calci
    int a,b used

---
+++ calci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"enter 1 num";
+cin>>a;
+
+int b;
+cout<<"enter 2 num";
+cin>>b;
+
+int sum;
+sum=a+b;
+cout<<sum;
+return 0;
+}
