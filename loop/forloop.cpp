6df1f88aa3e2894aec4ffd41a40bb0d375e5e0f6
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-03 15:30:01 +0530

    for loop

---
+++ loop/forloop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int i=1; 
+  while (i<=5){
+     cout<<i<<endl;
+     i++;
+  }
+ //example 1
+
+// int n;
+// cin>>n;
+// int sum=0;
+
+// int i=1;
+// while (i<=n){
+//     sum+=i;
+//     i++;
+// }
+//  cout<<sum<<endl;*/
+ return 0;
+}
