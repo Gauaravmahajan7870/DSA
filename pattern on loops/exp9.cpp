06036f700d85197549eb4b037030fdff16d61d6a
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-05 12:47:48 +0530

    loop example

---
+++ pattern on loops/exp9.cpp
@@ -0,0 +1,23 @@
+// Hollow Numerical Rectangle
+
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    int rows,columns;
+    cin>>rows>>columns;
+    for(int i=1; i <= rows; i++){
+        for(int j=1; j <= columns; j++){
+            if(i==1 || j==1 || i==rows || j==columns){
+                cout<<j;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+    
