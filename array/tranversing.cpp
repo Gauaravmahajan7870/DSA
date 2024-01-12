ee23a84614a0554fd71f7aca769372c0ff299f1c
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-12 20:59:57 +0530

    Update tranversing.cpp

--- array/tranversing.cpp
+++ array/tranversing.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main(){
 int array[]={1,2,3,4,5,6};
 int size =sizeof(array)/sizeof(array[0]);
-/*for (int ele:array){
+/*for (int ele:array){ //for each loop
   cout<<ele<<endl;
 }*/
 
-/*for (int idx=0;idx<size;idx++){
+/*for (int idx=0;idx<size;idx++){//for loop
   cout<<array[idx]<<endl;
   }*/
 
-  int idx=0;
+  int idx=0; //while loop
   while(idx<size){
     cout<<array[idx]<<endl;
     idx++;
