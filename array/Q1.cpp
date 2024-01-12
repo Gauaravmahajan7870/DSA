c75a0576a54300ab2f165a326792e8c193a558cc
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-12 22:30:07 +0530

    question

---
+++ array/Q1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int array[]={1,2,3,4,5,6};
+ int size =sizeof(array)/sizeof (array[0]);
+ int sum=0;
+ for (int idx=0;idx<size;idx++){
+     sum+=array[idx];
+ }
+ cout<<sum<<endl;
+ 
+ return 0;
+}
