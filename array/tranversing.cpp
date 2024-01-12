888bae6dbbf7662e91fe35eca8c98194b6e042f7
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-12 20:57:09 +0530

    traversing arrya

--- array/tranversing.cpp
+++ array/tranversing.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int 
+int array[]={1,2,3,4,5,6};
+int size =sizeof(array)/sizeof(array[0]);
+/*for (int ele:array){
+  cout<<ele<<endl;
+}*/
+
+/*for (int idx=0;idx<size;idx++){
+  cout<<array[idx]<<endl;
+  }*/
+
+  int idx=0;
+  while(idx<size){
+    cout<<array[idx]<<endl;
+    idx++;
+  }
 return 0;
 }
