236a064c5b544947295c375cef04127b2431996f
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-13 13:33:03 +0530

    for each loop

--- array/input.cpp
+++ array/input.cpp
@@ -6,11 +6,20 @@ char vowels[5];
     cin>>vowels[idx];
 }*/
 
-for (vowels:index){
-    
+/*for (vowels:index){
+
 }
 for (int idx=0;idx<5;idx++){
     cout<<vowels[idx]<<" ";
-}
+}*/
+ 
+ for (char &idx :vowels){
+    cin>>idx;
+ }
+ for (int idx=0;idx<5;idx++){
+    cout<<vowels[idx]<<" ";
+ }
+ 
+
 return 0;
 }
