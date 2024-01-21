ec92ce09cbeb6265a11ac03f4914e677c1867027
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-21 16:18:29 +0530

    vector
    llop pratice

---
+++ array/vectorloop.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int>v;
+for (int i=0;i<5;i++){
+    int ele;
+    cin>>ele;
+    v.push_back(ele);
+}
+for (int i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+}
+    cout<<endl;
+
+  for (int ele :v){
+    cout<<ele<<" ";
+
+  }
+  cout<<endl;
+
+  int idx=0;
+  while(idx<v.size()){
+    cout<<v[idx]<<" ";
+    idx++;
+  }
+  cout<<endl;
+
+return 0;
+}
