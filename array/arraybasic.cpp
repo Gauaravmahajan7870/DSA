9b31d6bc0f9236d5bd2b4a693ebee15538f20c3f
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-24 22:20:23 +0530

    array
    comments on basic

--- array/arraybasic.cpp
+++ array/arraybasic.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-char vowels[5];
+char vowels[5];                   //learn in this how to input array //#using loops
 /*for (int idx=0;idx<5;idx++){
     cin>>vowels[idx];
 }*/
 
-/*for (vowels:index){
+/*for (vowels:index){//for loop
 
 }
 for (int idx=0;idx<5;idx++){
     cout<<vowels[idx]<<" ";
 }*/
  
- for (char &idx :vowels){
+ for (char &idx :vowels){              //for each loop
     cin>>idx;
  }
  for (int idx=0;idx<5;idx++){
