79013d31c8ae87019eb5e21dbb87db277ed4b10f
Gauaravmahajan7870 <153429498+Gauaravmahajan7870@users.noreply.github.com>
2024-01-24 22:17:58 +0530

    array
    commentts added

--- array/vectorbasic.cpp
+++ array/vectorbasic.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 int main(){
  vector<int>v;
- cout<<"size :- "<<v.size()<<endl;
- cout<<"capacity :- "<<v.capacity()<<endl;
+ cout<<"size :- "<<v.size()<<endl;//find size of array
+ cout<<"capacity :- "<<v.capacity()<<endl;//find capacity of array
 
  
  v.push_back (1);
- cout<<"size : - "<<v.size()<<endl;
+ cout<<"size : - "<<v.size()<<endl;//push array means add elements
  cout<<"capacity : - "<<v.capacity()<<endl;
 
  v.push_back(2);
- cout<<"size:- "<<v.size()<<endl;
+ cout<<"size:- "<<v.size()<<endl;//push array means add elements
  cout<<"capacity :- "<<v.capacity()<<endl;
 
  v.push_back(3);
- cout<<"size:- "<<v.size()<<endl;
+ cout<<"size:- "<<v.size()<<endl;//push array means add elements
  cout<<"capacity :- "<<v.capacity()<<endl;
 
  v.push_back(4);
@@ -24,11 +24,11 @@ int main(){
  cout<<"capacity :- "<<v.capacity()<<endl;
 
  v.resize(5);
- cout<<"size:- "<<v.size()<<endl;
+ cout<<"size:- "<<v.size()<<endl;//resize array means change size of  elements
  cout<<"capacity :- "<<v.capacity()<<endl;
 
  v.pop_back();
- cout<<"pop size:- "<<v.size()<<endl;
+ cout<<"pop size:- "<<v.size()<<endl;////pop array means remove elements
  cout<<"pop capacity :- "<<v.capacity()<<endl;
 
 
